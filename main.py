66723c46c03240db5a37b534daeae0872dedd065
idoceb00 <idoceb00@estudiantes.unileon.es>
2025-04-18 22:02:12 +0200

    feature: generate the dataloaders for each of the fragments of the dataset

---
+++ main.py
@@ -0,0 +1,27 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+
+def get_cifar10_loaders(batch_size=64, validation_split=0.2):
+    transform = transforms.Compose([
+        transforms.Resize(224), # required for Transformer models
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), # CIFAR-10 mean
+                             (0.2023, 0.1994, 0.2010))  # CIFAR-10 std
+    ])
+
+    full_train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+
+
+    # Validation fragment size and training fragment size
+    validation_size = int(len(full_train_dataset) * validation_split)
+    train_size = len(full_train_dataset) - validation_size
+
+    train_dataset, val_dataset = random_split(full_train_dataset, [train_size, validation_size])
+
+    test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    validation_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    return train_loader, validation_loader, test_loader
